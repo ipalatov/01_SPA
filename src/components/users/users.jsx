1498289270b0fdbc076daf85e84379a3a51bd29b
Ivan <ivan.s.palatov@gmail.com>
2020-05-17 21:14:14 +0400

    lesson53 (class component Users)

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -4,47 +4,55 @@ import * as axios from 'axios';
 import userPhoto from '../../../src/assets/images/user.png'
 
 
-const Users = (props) => {
+class Users extends React.Component {
 
-    if (props.users.length === 0) {
+    constructor(props) {
+        super(props);
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users?page=10&count=5').then(response => {
-            props.setUsers(response.data.items);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users?page=20&count=5').then(response => {
+            this.props.setUsers(response.data.items);
         })
-
     }
 
-
-    const toggleFollowUser = (id, followed) => {
-        (followed) ? props.unFollowUser(id) : props.followUser(id)
+    toggleFollowUser = (id, followed) => {
+        (followed) ? this.props.unFollowUser(id) : this.props.followUser(id)
     }
 
-    return (
-        <div className={s.users_block}>
-            {props.users.map(us => (
-                <div key={us.id} className={s.item}>
-                    <span className={s.ib}>
-                        <div className={s.icon}>
-                            <img src={us.photos.small ? us.photos.small : userPhoto } alt='icon' />
-                        </div>
-                        <button onClick={() => toggleFollowUser(us.id, us.followed)}> {us.followed ? 'Unfollow' : 'Follow'} </button>
-                    </span>
-                    <div className={s.inline}>
-                        <span className={s.ns_cc}>
-                            <span className={s.name_status}>
-                                <div className={s.name}> {us.name} </div>
-                                <div>{us.status}</div>
-                            </span>
-                            <span className={s.locate}>
-                                <div>{"us.location.country"}</div>
-                                <div>{"us.location.city"}</div>
-                            </span>
+    render() {
+        return (
+
+            <div className={s.users_block}>
+                {this.props.users.map(us => (
+                    <div key={us.id} className={s.item}>
+                        <span className={s.ib}>
+                            <div className={s.icon}>
+                                <img src={us.photos.small ? us.photos.small : userPhoto} alt='icon' />
+                            </div>
+                            <button onClick={() => this.toggleFollowUser(us.id, us.followed)}> {us.followed ? 'Unfollow' : 'Follow'} </button>
                         </span>
-                    </div>
-                </div>))}
-        </div>
-    )
+                        <div className={s.inline}>
+                            <span className={s.ns_cc}>
+                                <span className={s.name_status}>
+                                    <div className={s.name}> {us.name} </div>
+                                    <div>{us.status}</div>
+                                </span>
+                                <span className={s.locate}>
+                                    <div>{"us.location.country"}</div>
+                                    <div>{"us.location.city"}</div>
+                                </span>
+                            </span>
+                        </div>
+                    </div>))}
 
+            </div>
+        )
+
+    }
 }
+
+
+
+
+
 
 export default Users;
